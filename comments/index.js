6d3c967b51aa3bfd531c0919c6b980898b51f698
Joe Stephen <joe.stephen@appsteamtechnologies.com>
2025-02-18 15:57:50 +0530

    Updated:query service added.

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { randomBytes } = require("crypto");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -13,15 +14,26 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
   res.status(201).send(comments);
 });
-
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body.type);
+  res.send({ status: "OK" });
+});
 app.listen(4001, () => {
   console.log("Comments listening on port 4001");
 });
