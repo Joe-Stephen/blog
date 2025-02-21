af63f9ec9a53680785a73264d6c03a74bc81a1ad
Joe Stephen <joe.stephen@appsteamtechnologies.com>
2025-02-21 14:49:53 +0530

    Added:docker files added.

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,38 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {
-  const event = req.body;
-  axios.post("http://localhost:4000/events", event); //posts
-  axios.post("http://localhost:4001/events", event); //comments
-  axios.post("http://localhost:4002/events", event); //query
-  axios.post("http://localhost:4003/events", event); //moderation
+const events = [];
 
-  res.send({ status: "OK" });
+app.post("/events", async (req, res) => {
+  const event = req.body;
+  events.push(event);
+  const eventEndpoints = [
+    "http://localhost:4000/events", // posts
+    "http://localhost:4001/events", // comments
+    "http://localhost:4002/events", // query
+    "http://localhost:4003/events", // moderation
+  ];
+  try {
+    const results = await Promise.allSettled(
+      eventEndpoints.map((url) => axios.post(url, event))
+    );
+    results.forEach((result, index) => {
+      if (result.status === "rejected") {
+        console.error(
+          `Error sending event to ${eventEndpoints[index]}:`,
+          result.reason.message
+        );
+      }
+    });
+    res.send({ status: "OK" });
+  } catch (error) {
+    console.error("Unexpected error in /events POST API:", error);
+    res.status(500).send({ status: "ERROR", message: "Internal Server Error" });
+  }
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
